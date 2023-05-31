4b899c59ed6f7f9f3d93e97a7307b70f42d09faa
piyush <piyushsbbs@gmail.com>
2023-05-31 21:46:06 +0530

    test

---
+++ src/main/java/pojo/requestpojo.java
@@ -0,0 +1,63 @@
+package pojo;
+
+import java.util.List;
+
+public class requestpojo {
+	private locationpojo location;
+	private Integer accuracy;
+	private String name;
+	private String phone_number;
+	private String address;
+	private List<String> types;
+	private String website;
+	private String language;
+	public locationpojo getLocation() {
+		return location;
+	}
+	public void setLocation(locationpojo location) {
+		this.location = location;
+	}
+	public Integer getAccuracy() {
+		return accuracy;
+	}
+	public void setAccuracy(Integer accuracy) {
+		this.accuracy = accuracy;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public List<String> getTypes() {
+		return types;
+	}
+	public void setTypes(List<String> types) {
+		this.types = types;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
+}
