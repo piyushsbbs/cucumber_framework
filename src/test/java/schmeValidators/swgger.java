eff59ad29b8845754ca8240a3835539d6dcfc2af
piyush <piyushsbbs@gmail.com>
2023-06-01 10:37:19 +0530

    fixing

--- src/test/java/schmeValidators/swgger.java
+++ src/test/java/schmeValidators/swgger.java
@@ -8,24 +8,16 @@ import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInC
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
+
+import io.restassured.matcher.RestAssuredMatchers;
 public class swgger {
 	@Test
 	public void schema(){
-//		Adding branch in git branch
 		RestAssured.baseURI="https://petstore.swagger.io";
 		Response str=given().log().all().header("Accept","application/xml").queryParam("status", "available")
 		.when().get("v2/pet/findByStatus");
 		
-		str.then().assertThat().statusCode(200).assertThat().log().all().body(matchesXsdInClasspath("demoxsd.xsd"));
-		RestAssured.baseURI="https://petstore.swagger.io";
-		
+		str.then().assertThat().statusCode(200).log().all().body(matchesXsdInClasspath("demoxsd.xsd"));
 	}
-@Test
-public void jsonvalidator()
-{
-	Response str2=given().log().all().header("Accept","application/xml").queryParam("status", "available")
-			.when().get("v2/pet/findByStatus");
-			str2.then().assertThat().statusCode(200).assertThat().log().all().body(matchesJsonSchemaInClasspath("jsonschema.json"));
-}
 
 }
