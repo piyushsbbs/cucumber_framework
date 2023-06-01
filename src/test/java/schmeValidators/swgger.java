9b504df0044a21db10780fd2688b67fe0ca30159
piyush <piyushsbbs@gmail.com>
2023-06-01 10:29:03 +0530

    added test to valiate jason schema

--- src/test/java/schmeValidators/swgger.java
+++ src/test/java/schmeValidators/swgger.java
@@ -18,9 +18,14 @@ public class swgger {
 		
 		str.then().assertThat().statusCode(200).assertThat().log().all().body(matchesXsdInClasspath("demoxsd.xsd"));
 		RestAssured.baseURI="https://petstore.swagger.io";
-		Response str2=given().log().all().header("Accept","application/xml").queryParam("status", "available")
-		.when().get("v2/pet/findByStatus");
-		str2.then().assertThat().statusCode(200).assertThat().log().all().body(matchesJsonSchemaInClasspath("jsonschema.json"));
+		
 	}
+@Test
+public void jsonvalidator()
+{
+	Response str2=given().log().all().header("Accept","application/xml").queryParam("status", "available")
+			.when().get("v2/pet/findByStatus");
+			str2.then().assertThat().statusCode(200).assertThat().log().all().body(matchesJsonSchemaInClasspath("jsonschema.json"));
+}
 
 }
